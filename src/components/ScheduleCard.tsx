a843239b93d18829f5037922cb70e5d68216385d
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2023-06-21 02:08:41 +0530

    Fixed minor bugs

--- src/components/ScheduleCard.tsx
+++ src/components/ScheduleCard.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { ScheduleItem } from "~/types";
 
 const ScheduleCard = ({ task }: any) => {
-	console.log(task);
 	return (
 		<div className="flex items-center gap-4">
-			{/* <div className="h-[3.5rem] bg-[#6972C3]"> */}
-
-			<div className={`h-[3.5rem] bg-[${task.color}]`}>
+			<div className={`h-[3.5rem] ${task.color}`}>
 				&nbsp;
 				<br />
 				&nbsp;
 				<br />
 			</div>
 			<div className="flex flex-col gap-1 text-xs opacity-70 hover:opacity-100">
-				{/* <h1 className="text-sm font-bold">Meeting with suppliers</h1> */}
 				<h1 className="text-sm font-bold">{task.title}</h1>
 				<p>{task.time}</p>
 				<p>
--- src/components/StatusCardsContainer.tsx
+++ src/components/StatusCardsContainer.tsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StatusCards from "./StatusCards";
+import socials from "../../public/statussvg/socials.svg";
+import payment from "../../public/statussvg/Payment.svg";
+import like from "../../public/statussvg/like.svg";
+import tag from "../../public/statussvg/tagSvg.svg";
+import Image from "next/image";
 
 const StatusCardsContainer = () => {
+	const [Values, setValues] = useState<any[]>([]);
+
+	useEffect(() => {
+		const values = [
+			{
+				image: socials,
+				color: "bg-[#DEE0EF]",
+				title: "Total Users",
+				Value: "892",
+			},
+			{
+				image: payment,
+				color: "bg-[#DDEFE0]",
+				title: "Total Revenues",
+				Value: "$2,129,430",
+			},
+			{
+				image: like,
+				color: "bg-[#EFDADA]",
+				title: "Total Likes",
+				Value: "9,721",
+			},
+			{
+				image: tag,
+				color: "bg-[#F4ECDD]",
+				title: "Total Transactions",
+				Value: "1,520",
+			},
+		];
+
+		setValues(values);
+	}, []);
 	return (
 		<div className="my-8 flex gap-4">
-			<StatusCards />
-			<StatusCards />
-			<StatusCards />
-			<StatusCards />
+			{Values.map((elem: any) => (
+				<StatusCards items={elem} key={elem.title} color={elem.color}/>
+			))}
 		</div>
 	);
 };
